a259f0b0a2aaa243075a24e3fff46a3cf2b6bc56
Grzegorz <grzcichosz@gmail.com>
2022-03-06 11:07:32 +0100

    task4 solution with 2 replacements

---
+++ src/pl/sdacademy/java/basic/exercises/day2/Task4.java
@@ -0,0 +1,19 @@
+package pl.sdacademy.java.basic.exercises.day2;
+
+public class Task4 {
+    private static final String REPLACEMENT = "-STOP-";
+    private static final String REGEX = "[.,]";
+
+    public static void main(String[] args) {
+        String input = "Ala, ma kota. Kot, ma Ale";
+        System.out.println(replaceText(input));
+    }
+
+    private static String replaceText(String input){
+        if(StringHelper.isValid(input)){
+            //return input.replace(".", REPLACEMENT).replace(",", REPLACEMENT);
+            return input.replaceAll(REGEX, REPLACEMENT);
+        }
+        return input;
+    }
+}
